c569a67b846c555dc4be1e5a27554b50a554cc1f
nguyen <daudaua3782@gmail.com>
2020-08-17 10:00:54 +0700

    Re-logic Runnable

--- src/main/java/com/playernguyen/schedule/AuthesForceLogin.java
+++ src/main/java/com/playernguyen/schedule/AuthesForceLogin.java
@@ -20,38 +20,48 @@ public class AuthesForceLogin extends AuthesRunnable {
 
     @Override
     public void run() {
+        // Update tick
         this.ticker --;
-        if (player != null) {
-            if (getAccountManager().getAccountFromUUID(getPlayer().getUniqueId()) == null) {
-                this.cancel();
-                return;
-            }
-            // Handle information of login/register
-            if (!getAccountManager().getAccountFromUUID(getPlayer().getUniqueId()).isRegistered()) {
-                // Anti spam
-                if (ticker % 2 == 0) {
-                    this.getPlayer().sendMessage(getLanguage().get(LanguageFlag.REQUIRE_REGISTER));
-                    return;
-                }
-            } else if (!getSessionManager().hasSession(player.getUniqueId())) {
-                if (ticker % 2 == 0) {
-                    this.getPlayer().sendMessage(getLanguage().get(LanguageFlag.REQUIRE_LOGIN));
-                    return;
-                }
-            } else {
-                this.cancel();
-                this.getPlayer().sendMessage(getLanguage().get(LanguageFlag.LOGIN_SUCCESS));
-                return;
-            }
 
-            // If out of time (idle), kick player
-            if (ticker <= 0) {
-                this.getPlayer().kickPlayer(this.getLanguage().get(LanguageFlag.KICK_REASON));
-                // Cancel the task too
-                this.cancel();
-            }
-        } else {
+        // Player not found
+        if (player == null) {
             this.cancel();
+            return;
         }
+
+        // Not found account
+        if (getAccountManager().getAccountFromUUID(getPlayer().getUniqueId()) == null) {
+            this.cancel();
+            return;
+        }
+
+        // Is not register
+        if (!getAccountManager().getAccountFromUUID(getPlayer().getUniqueId()).isRegistered()) {
+            // Anti spam
+            if (ticker % 2 == 0) {
+                this.getPlayer().sendMessage(getLanguage().get(LanguageFlag.REQUIRE_REGISTER));
+            }
+            return;
+        }
+
+        // Not has session
+        if (!getSessionManager().hasSession(player.getUniqueId())) {
+            if (ticker % 2 == 0) {
+                this.getPlayer().sendMessage(getLanguage().get(LanguageFlag.REQUIRE_LOGIN));
+
+            }
+            return;
+        }
+
+        // If out of time (idle), kick player
+        if (ticker <= 0) {
+            this.getPlayer().kickPlayer(this.getLanguage().get(LanguageFlag.KICK_REASON));
+            // Cancel the task too
+            this.cancel();
+            return;
+        }
+
+        this.getPlayer().sendMessage(getLanguage().get(LanguageFlag.LOGIN_SUCCESS));
+        this.cancel();
     }
 }
